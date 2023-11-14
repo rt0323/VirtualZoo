d12bede2fea9f17a50fb13245615e8f4d0a25b8b
rt0323 <137693743+rt0323@users.noreply.github.com>
2023-11-14 09:14:36 +0600

    web.php

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/info', [App\Http\Controllers\ZooController::class, 'info'])->name('info');
-Route::get('/edit', [App\Http\Controllers\ZooController::class, 'edit'])->name('edit');
-Route::get('/create', [App\Http\Controllers\ZooController::class, 'Create'])->name('create');
-Route::post('/create', [App\Http\Controllers\ZooController::class, 'AddZoo'])->name('create');
-Route::get('edit/{id}', [App\Http\Controllers\ZooController::class, 'update']);
-Route::get('delete/{id}', [App\Http\Controllers\ZooController::class, 'delete']);
-Route::put('updatezoo/{id}', [App\Http\Controllers\ZooController::class, 'zoo']);
+Route::get('/edit', [App\Http\Controllers\ZooController::class, 'edit'])->name('edit')->middleware('auth');
+Route::get('/create', [App\Http\Controllers\ZooController::class, 'Create'])->name('create')->middleware('auth');
+Route::post('/create', [App\Http\Controllers\ZooController::class, 'AddZoo'])->name('create')->middleware('auth');
+Route::get('edit/{id}', [App\Http\Controllers\ZooController::class, 'update'])->middleware('auth');
+Route::get('delete/{id}', [App\Http\Controllers\ZooController::class, 'delete'])->middleware('auth');
+Route::put('updatezoo/{id}', [App\Http\Controllers\ZooController::class, 'zoo'])->middleware('auth');
 Route::get('/', [App\Http\Controllers\ZooController::class, 'animal'])->name('animal');
